c3a29646eca3c6f38459cdda2d6a60df68d9b830
dtmkeng <kang081@hotmail.com>
2019-05-28 21:22:36 +0700

    update service

--- fontend/build/main.js
+++ fontend/build/main.js
@@ -1,8 +1,8 @@
 console.log('hellojs')
-const host_home = "home-service"
-const host_car = "car-service"
-// const host_home = "localhost"
-// const host_car = "localhost"
+// const host_home = "home-service"
+// const host_car = "car-service"
+const host_home = "localhost"
+const host_car = "localhost"
 const port_home = 9000;
 const port_car = 9001;
 fetch(`http://${host_home}:${port_home}/`).then(res=>res.json()).then(data=>{

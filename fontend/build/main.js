9f889ef93ab7d53adea3b75aa48a755ba17123ab
dtmkeng <kang081@hotmail.com>
2019-05-30 00:05:23 +0700

    add

--- fontend/build/main.js
+++ fontend/build/main.js
@@ -1,11 +1,11 @@
 console.log('hellojs')
-// const host_home = "home-service"
-// const host_car = "car-service"
-const host_home = "localhost"
-const host_car = "localhost"
+const host_home = "backend2"
+const host_car  = "backend1"
+// const host_home = "localhost"
+// const host_car = "localhost"
 const port_home = 9000;
 const port_car = 9001;
-fetch(`http://${host_home}:${port_home}/`).then(res=>res.json()).then(data=>{
+fetch(`http://${host_home}:${port_home}`).then(res=>res.json()).then(data=>{
     // console.log(data)
     console.log(data)
     const sel = document.getElementById('home-service');
@@ -21,7 +21,7 @@ fetch(`http://${host_home}:${port_home}/`).then(res=>res.json()).then(data=>{
 // Typehome: "single"
     
 })
-fetch(`http://${host_car}:${port_car}/`).then(res=>res.json()).then(data=>{
+fetch(`http://${host_car}:${port_car}`).then(res=>res.json()).then(data=>{
     // console.log(data)
     console.log(data)
     const sel = document.getElementById('car-service');
